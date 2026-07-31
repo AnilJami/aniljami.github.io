7ee628f66b24af737e4655c9228e32b02175fcd4
Anil Jami <Anil.Jami@thomsonreuters.com>
2026-07-30 22:46:01 -0500

    bell

--- public/js/data.js
+++ public/js/data.js
@@ -53,6 +53,65 @@ async function getVisitorDetails() {
 
 // Execute when the page hits / loads
 getVisitorDetails().then(details => {
-    const messageElement = document.getElementById("locData");
-    messageElement.textContent = JSON.stringify( details, null, 2);
+    //const messageElement = document.getElementById("locData");
+    //messageElement.textContent = JSON.stringify( details, null, 2);
 });
+
+
+let myImage = document.getElementById("myImage");
+let webhookUrl = "__WEBHOOK_URL_TEAMS__";
+webhookUrl = webhookUrl + "&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MP4Rni_6pRwKUZsZpCyMpcRtVw584sbD2qtWWkKwl_E"
+// 1. Define the function that changes the text
+function changeWelcomeMessage() {
+    // Find the element by its unique ID
+    const messageElement = document.getElementById("myMessage");
+    const params = new URLSearchParams(window.location.search);
+
+    const browserData = {
+        platform: navigator.platform,
+        screenResolution: `${window.screen.width}x${window.screen.height}`
+    };
+    let data = {
+        mobileData: getMobileDeviceDetails(),
+        browser: browserData,
+    }
+    const payload = {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": {
+                    "type": "AdaptiveCard",
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "text": JSON.stringify( data, null, 2)
+                        }
+                    ],
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "version": "1.2"
+                }
+            }
+        ]
+    }
+    fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+        .then(() => {
+            myImage.style.display = "block";
+            messageElement.style.display = "none";
+            //messageElement.textContent = "✅ Success 🔔 : " + params
+        })
+        .catch(error => {
+            console.log(error)
+            messageElement.textContent = "❌ Failed"
+        } );
+
+}
+
+// 2. Trigger the function as soon as the HTML document is fully loaded
+document.addEventListener("DOMContentLoaded", changeWelcomeMessage);
